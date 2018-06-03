c6bee66f1ff02186c2b9389484988a79c9297a8b
Ahmad Raza <raza2157@gmail.com>
2018-06-03 19:05:18 -0400

    Added eslint

--- React/src/app/index.js
+++ React/src/app/index.js
@@ -1,13 +1,16 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react'
+import { render } from 'react-dom'
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
-      <div>
-        <h1>Hello!</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1" />
+          <h1>Hello!</h1>
+        </div>
       </div>
-    );
+    )
   }
 }
-render(<App />, window.document.getElementById("app"));
+render(<App />, window.document.getElementById('app'))
