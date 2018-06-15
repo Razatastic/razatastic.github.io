de1da8436aa4946c69bd3b7f8eae9ffda9f8a4a0
Ahmad Raza <raza2157@gmail.com>
2018-06-15 02:14:32 -0400

    Replaced original website with a new one (Major commit)
    - Removed OG folder
    - Created new index.html file
    - Setup src directory with required files

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+// Loads Feather Icons
+feather.replace();
+
+// Particles.js Configuration
+window.onload = function () {
+    Particles.init({
+        selector: '.background',
+        color: '#6B5B95',
+        connectParticles: 'true',
+        responsive: [
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 60
+                }
+            }, {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 30
+                }
+            }, {
+                breakpoint: 320,
+                options: {
+                    maxParticles: 0
+                }
+            }
+        ]
+    });
+};
