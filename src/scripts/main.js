bcc3c9a5266d6a84cabc0096c4267ba8a9c2d9cc
Ahmad Raza <raza2157@gmail.com>
2018-06-17 01:52:24 -0400

    Added Animate.css, animations to social icons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,28 +2,36 @@
 feather.replace();
 
 // Particles.js Configuration
-window.onload = function () {
-    Particles.init({
-        selector: '.background',
-        color: '#aaaaff',
-        connectParticles: 'true',
-        responsive: [
-            {
-                breakpoint: 768,
-                options: {
-                    maxParticles: 60
-                }
-            }, {
-                breakpoint: 425,
-                options: {
-                    maxParticles: 30
-                }
-            }, {
-                breakpoint: 320,
-                options: {
-                    maxParticles: 0
-                }
-            }
-        ]
-    });
+window.onload = function() {
+  Particles.init({
+    selector: ".background",
+    color: "#aaaaff",
+    connectParticles: "true",
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+          maxParticles: 60
+        }
+      },
+      {
+        breakpoint: 425,
+        options: {
+          maxParticles: 30
+        }
+      },
+      {
+        breakpoint: 320,
+        options: {
+          maxParticles: 0
+        }
+      }
+    ]
+  });
 };
+
+// Social Icons animation
+setTimeout(function() {
+    document.getElementById("greeting_icons").style.visibility = "visible";
+    $('#greeting_icons').addClass('animated lightSpeedIn');
+}, 1800);
