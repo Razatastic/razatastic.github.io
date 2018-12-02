606efc8f95f7aa0afc3a7e90b5155a1d951c2ba8
Ahmad Raza <raza2157@gmail.com>
2018-12-02 18:56:53 -0500

    Linting

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,27 +1,27 @@
 // Particles.js Configuration
 window.onload = function() {
-    Particles.init({
-      selector: ".background",
-      maxParticles: 90,
-      responsive: [
-        {
-          breakpoint: 768,
-          options: {
-            maxParticles: 60
-          }
-        },
-        {
-          breakpoint: 425,
-          options: {
-            maxParticles: 30
-          }
-        },
-        {
-          breakpoint: 320,
-          options: {
-            maxParticles: 0
-          }
+  Particles.init({
+    selector: ".background",
+    maxParticles: 90,
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+          maxParticles: 60
         }
-      ]
-    });
-  };
+      },
+      {
+        breakpoint: 425,
+        options: {
+          maxParticles: 30
+        }
+      },
+      {
+        breakpoint: 320,
+        options: {
+          maxParticles: 0
+        }
+      }
+    ]
+  });
+};
