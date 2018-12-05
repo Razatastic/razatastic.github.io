e372a3d7daf8b612988b5c7621f78be97cffab88
Ahmad Raza <raza2157@gmail.com>
2018-12-05 00:02:57 -0500

    Added titles, updated JS

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -25,3 +25,23 @@ window.onload = function() {
     ]
   });
 };
+
+// Alert Animation
+var playButtons = document.getElementsByClassName("playButton");
+
+for (i = 0; i < playButtons.length; i++) {
+  // Add event listeners for all play buttons
+  playButtons[i].addEventListener("click", showAlert);
+}
+
+function showAlert() {
+  var alertElement = document.getElementById("comingSoonAlert");
+  alertElement.classList.remove("d-none");
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+document.getElementById("closeButton").addEventListener("click", hideAlert);
+function hideAlert() {
+  var alertElement = document.getElementById("comingSoonAlert");
+  alertElement.classList.add("d-none");
+}
