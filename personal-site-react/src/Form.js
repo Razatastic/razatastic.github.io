78c7fbfee0f0c1471dabd34fc11458c739e48d10
Ahmad Raza <raza2157@gmail.com>
2019-01-24 15:04:00 -0500

    Continued tutorial

---
+++ personal-site-react/src/Form.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = {
+      name: "",
+      job: ""
+    };
+    this.state = this.initialState;
+  }
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+}
+
+export default Form;
